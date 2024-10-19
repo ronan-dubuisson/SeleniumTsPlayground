a61ac33666ca3487b50687d4c92ad14cca515961
Ronan Dubuisson <ronan.dubuisson@dubuissonconsulting.be>
2024-10-20 01:15:37 +0200

    revert from pages branch

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -1,13 +1,13 @@
 import {after, afterEach, beforeEach, before} from 'mocha';
 import {expect} from 'chai';
 import {Builder} from 'selenium-webdriver';
-import {LoginPage} from '../pom/login.page.js';
 import addContext from 'mochawesome/addContext.js';
+import { LoginPage } from '../pom/login.page';
 
 describe('login', function () {
   //test suite
   this.timeout(10000);
-  let loginpage: LoginPage;
+  let loginPage: LoginPage;
 
   before(async function () {}); //before test suite
 
@@ -16,113 +16,113 @@ describe('login', function () {
   beforeEach(async function () {
     //setup
     const browser = new Builder().forBrowser('chrome').build();
-    loginpage = new LoginPage(browser);
-    loginpage.visit('https://www.saucedemo.com/');
-    loginpage.validatePage();
+    loginPage = new LoginPage(browser);
+    loginPage.visit('https://www.saucedemo.com/');
+    loginPage.validatePage();
   }); //before each test
 
   afterEach(async function () {
     if (this.currentTest?.state !== 'passed') {
       addContext(
         this,
-        await loginpage.takeScreenshot(this.currentTest?.fullTitle()),
+        await loginPage.takeScreenshot(this.currentTest?.fullTitle()),
       );
     }
 
-    loginpage.close();
+    loginPage.close();
   }); //after each test
 
   it('check username placeholder text', async function () {
     expect(
-      await loginpage.userNamePlaceholder(),
-      `Placeholder should be ${loginpage.USERNAME_PLACEHOLDER}`,
-    ).equal(loginpage.USERNAME_PLACEHOLDER);
+      await loginPage.userNamePlaceholder(),
+      `Placeholder should be ${loginPage.USERNAME_PLACEHOLDER}`,
+    ).equal(loginPage.USERNAME_PLACEHOLDER);
   });
 
   it('check password placeholder text', async function () {
     expect(
-      await loginpage.passwordPlaceholder(),
-      `Placeholder should be ${loginpage.PASSWORD_PLACEHOLDER}`,
-    ).equal(loginpage.PASSWORD_PLACEHOLDER);
+      await loginPage.passwordPlaceholder(),
+      `Placeholder should be ${loginPage.PASSWORD_PLACEHOLDER}`,
+    ).equal(loginPage.PASSWORD_PLACEHOLDER);
   });
 
   it('login with no credentials', async function () {
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
 
     expect(
-      await loginpage.loginErrorMessage(),
-      `Error message should be ${loginpage.ERROR_NO_CREDENTIALS}`,
-    ).equal(loginpage.ERROR_NO_CREDENTIALS);
+      await loginPage.loginErrorMessage(),
+      `Error message should be ${loginPage.ERROR_NO_CREDENTIALS}`,
+    ).equal(loginPage.ERROR_NO_CREDENTIALS);
   });
 
   it('login with missing password', async function () {
-    await loginpage.enterUsername('standar_user');
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
+    await loginPage.enterUsername('standar_user');
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
 
     expect(
-      await loginpage.loginErrorMessage(),
-      `Error message should be ${loginpage.ERROR_ONLY_USERNAME}`,
-    ).equal(loginpage.ERROR_ONLY_USERNAME);
+      await loginPage.loginErrorMessage(),
+      `Error message should be ${loginPage.ERROR_ONLY_USERNAME}`,
+    ).equal(loginPage.ERROR_ONLY_USERNAME);
   });
 
   it('login with missing username', async function () {
-    await loginpage.enterPsw('secret_sauce');
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
+    await loginPage.enterPsw('secret_sauce');
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
 
     expect(
-      await loginpage.loginErrorMessage(),
-      `Error message should be ${loginpage.ERROR_ONLY_PASSWORD}`,
-    ).equal(loginpage.ERROR_ONLY_PASSWORD);
+      await loginPage.loginErrorMessage(),
+      `Error message should be ${loginPage.ERROR_ONLY_PASSWORD}`,
+    ).equal(loginPage.ERROR_ONLY_PASSWORD);
   });
 
   it('login with wrong credentials', async function () {
-    await loginpage.enterUsername('wrong username');
-    await loginpage.enterPsw('wrong password');
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
+    await loginPage.enterUsername('wrong username');
+    await loginPage.enterPsw('wrong password');
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
 
     expect(
-      await loginpage.loginErrorMessage(),
-      `Error message should be ${loginpage.ERROR_WRONG_CREDENTIALS}`,
-    ).equal(loginpage.ERROR_WRONG_CREDENTIALS);
+      await loginPage.loginErrorMessage(),
+      `Error message should be ${loginPage.ERROR_WRONG_CREDENTIALS}`,
+    ).equal(loginPage.ERROR_WRONG_CREDENTIALS);
   });
 
   it('close login error message', async function () {
-    await loginpage.enterUsername('wrong username');
-    await loginpage.enterPsw('wrong password');
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
-    await loginpage.closeErrorMessage();
+    await loginPage.enterUsername('wrong username');
+    await loginPage.enterPsw('wrong password');
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
+    await loginPage.closeErrorMessage();
 
     expect(
-      await loginpage.errorIsVisible(),
+      await loginPage.errorIsVisible(),
       'Error message should not be visible after being closed',
     ).equal(false);
   });
 
   it('login standard user', async function () {
-    await loginpage.enterUsername('standard_user');
-    await loginpage.enterPsw('secret_sauce');
-    await loginpage.clickLoginButton();
+    await loginPage.enterUsername('standard_user');
+    await loginPage.enterPsw('secret_sauce');
+    await loginPage.clickLoginButton();
 
     expect(
-      await loginpage.succesLoginPage(),
+      await loginPage.succesLoginPage(), //TODO: change once product page is existing
       'Invetory page should be shown afer successfull login',
     );
   });
 
   it('login locked out user', async function () {
-    await loginpage.enterUsername('locked_out_user');
-    await loginpage.enterPsw('secret_sauce');
-    await loginpage.clickLoginButton();
-    await loginpage.waitForErrorMessage();
+    await loginPage.enterUsername('locked_out_user');
+    await loginPage.enterPsw('secret_sauce');
+    await loginPage.clickLoginButton();
+    await loginPage.waitForErrorMessage();
 
     expect(
-      await loginpage.loginErrorMessage(),
-      `Error message should be ${loginpage.ERROR_LOCKED_USER}`,
-    ).equal(loginpage.ERROR_LOCKED_USER);
+      await loginPage.loginErrorMessage(),
+      `Error message should be ${loginPage.ERROR_LOCKED_USER}`,
+    ).equal(loginPage.ERROR_LOCKED_USER);
   });
 });
